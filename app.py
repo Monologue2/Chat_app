faf47bd8e0ba25bfd329cdea3f5518a802cffc64
Monologue2 <seyoumoon@gmail.com>
2023-02-08 17:26:25 +0900

    modify 1.0

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    socketio.run(app, host = "0.0.0.0")
+    socketio.run(app, host = "127.0.0.1", allow_unsafe_werkzeug=True)
